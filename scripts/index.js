6f6a5056ef15c384620a6581f9b6b53b6ce646bb
Yulia Jaworski <yuliashusharinav@yandex.ru>
2023-01-21 17:19:59 +0500

    fix: исправить ошибки ревью 1

--- scripts/index.js
+++ scripts/index.js
@@ -2,33 +2,33 @@ console.log('Привет, мир!');
 const popupElement = document.querySelector('.popup');
 const popupCloseElement = popupElement.querySelector('.popup__close');
 const popupOpenElement = document.querySelector('.profile__edit-button');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__job');
+let formElement = document.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__element_name');
+let jobInput = formElement.querySelector('.popup__element_job');
 
 const openPopup = function() {
-    popupElement.classList.add('popup__opened');
+    popupElement.classList.add('popup_opened');
+
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
 }
 
 const closePopup = function() {
-    popupElement.classList.remove('popup__opened');
+    popupElement.classList.remove('popup_opened');
 }
 
 popupOpenElement.addEventListener('click', openPopup);
 popupCloseElement.addEventListener('click', closePopup);
 
-let formElement = document.querySelector('.popup__form');
-let nameInput = formElement.querySelector('.popup__name');
-let jobInput = formElement.querySelector('.popup__job');
-
 function handleFormSubmit (evt) {
     evt.preventDefault();
 
-    nameInput.getAttribute('value');
-    jobInput.getAttribute('value');
-
-    let profileName = document.querySelector('.profile__name');
-    let profileJob = document.querySelector('.profile__job');
-
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
+
+    closePopup ();
 }
 
 formElement.addEventListener('submit', handleFormSubmit);
